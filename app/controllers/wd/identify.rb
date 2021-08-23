146d473ac41f50502b7adafe9165f5e780ae4769
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-24 01:49:19 +0800

    suport decode auth token

--- app/controllers/wd/identify.rb
+++ app/controllers/wd/identify.rb
@@ -10,5 +10,23 @@ module Wd
 
     end
 
+    def auth_token
+      return @auth_token if defined? @auth_token
+      @auth_token = request.headers['Authorization'].to_s.split(' ').last.presence
+    end
+
+    def decode_auth_token
+      begin
+        payload, _ = JWT.decode(auth_token, nil, false, verify_expiration: false)
+        return unless payload
+        key = 'abcdefghijklmn'  #fixme 在 auth 后台配置
+
+        payload, _ = JWT.decode(auth_token, key, true, 'sub' => payload['sub'], verify_sub: true, verify_expiration: false)
+        payload['sub'].constantize.find payload['iss']
+      rescue => e
+        logger.debug e.full_message(highlight: true, order: :top)
+      end
+    end
+
   end
 end
